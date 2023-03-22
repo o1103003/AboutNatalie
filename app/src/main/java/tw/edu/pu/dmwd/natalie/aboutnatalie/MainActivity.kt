cf0abaf3f4dd41d6a1a95f5754d20a09829dab9c
Natalie <o1103003@gm.pu.edu.tw>
2023-03-22 15:08:53 +0800

    About

---
+++ app/src/main/java/tw/edu/pu/dmwd/natalie/aboutnatalie/MainActivity.kt
@@ -0,0 +1,35 @@
+package tw.edu.pu.dmwd.natalie.aboutnatalie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var txv: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txv = findViewById(R.id.txv)
+        txv.setOnClickListener({
+            if (txv.text == "Natalie 您好!"){
+                txv.text = "海青班很棒,數媒更是好的方向"
+            }
+            else{
+                txv.text = "Natalie 您好!"
+            }
+        })
+    }
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        var txv: TextView = findViewById(R.id.txv)
+
+        txv.text = "螢幕觸控"
+
+        return true
+
+    }
+
+
+}
